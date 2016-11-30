202030d6d0dfd3eb2d8bf2381c7d346abefd1281
Gabriel Klockner <gabriel.klockner@gmail.com>
2016-11-30 20:46:09 -0200

    Adds support to mailcatcher and new signup email test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :rooms
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope ":locale", locale: /en|pt/ do
+    resources :rooms
+    resources :users
+  end
 
+  get '/:locale' => 'home#index', locale: /en|pt/
   root 'rooms#index'
 end
